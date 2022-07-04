c8866bc3bb5b553876ad1773fb5586c3b536008c
iavianm <iavianm@mail.ru>
2022-07-04 16:23:06 +0300

    second lesson

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def index
-    @username = params[:username]
     render 'pages/index' #необязательный параметр(по умолчанию это и так срендериться)
   end
 end
