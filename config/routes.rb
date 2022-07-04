f444603d7906b964dd4cfaad3705158f9985b94a
iavianm <iavianm@mail.ru>
2022-07-04 22:10:46 +0300

    third lesson

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  get '/questions', to: 'questions#index'
+  resources :questions
+  # resources :questions, only: [:index, :new, :edit, :create, :update, :destroy, :show]
+  # get '/questions', to: 'questions#index'
+  # get '/questions/new', to: 'questions#new'
+  # get '/questions/:id/edit', to: 'questions#edit'
+  # post '/questions', to: 'questions#create'
 end
